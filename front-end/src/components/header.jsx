f7701e3257b8a5dc9931ac40220e2ed9a94aa57d
Gustavo Martins dos Santos <santos.gustavo@edu.univali.br>
2020-10-02 06:52:28 -0300

    1.0

---
+++ front-end/src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => (
+    <div id="NavBar">
+        <div class="NavItem">
+            TASKS¡
+        </div>
+        <div class="NavItem">
+            Início
+        </div>
+        <div class="NavItem">
+            Tarefas
+        </div>
+        <div class="NavItem">
+            Relatórios
+        </div>
+    </div>
+)
---
+++ front-end/src/components/tasksList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function processTasks(){
+
+}
+
+export default () => (
+    <div id="tasks-list">
+        <div class="task-item">
+            <div class="task-itemTitle">
+                [OC=1028] Tarefa 1
+            </div>
+        </div>
+    </div>
+)
